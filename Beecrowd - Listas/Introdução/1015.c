0f5cf6a72d9e536cf1d7339647b5f05a5cfb963e
João Pedro de França Barboza <77846057+JoaoPFranca@users.noreply.github.com>
2023-08-30 10:10:23 -0300

    finalizando a lista 1

---
+++ Beecrowd - Listas/Introdução/1015.c
@@ -0,0 +1,25 @@
+// Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula
+
+// O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto flutuante: x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+// Saída
+
+// Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  double x1;
+  double y1;
+  double x2;
+  double y2;
+  double formula;
+
+  scanf("%lf %lf", &x1, &y1);
+  scanf("%lf %lf", &x2, &y2);
+
+  formula = sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+  printf("%.4lf\n", formula);
+  
+}
